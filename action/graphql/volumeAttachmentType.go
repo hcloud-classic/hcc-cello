b1224a593df48d92d68550f724c62e4236e5e93d
hcloudclassic <hcloudclassic@innogrid.com>
2019-10-22 18:33:13 +0900

    update architecture edit graphql query

--- action/graphql/volumeAttachmentType.go
+++ action/graphql/volumeAttachmentType.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:action/graphql/volumeAttachmentType.go
 package graphql
 
 import "github.com/graphql-go/graphql"
@@ -24,3 +25,37 @@ var volumeAttachmentType = graphql.NewObject(
 		},
 	},
 )
+=======
+package graphql
+
+import "github.com/graphql-go/graphql"
+
+var volumeType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Volume",
+		Fields: graphql.Fields{
+			"uuid": &graphql.Field{
+				Type: graphql.String,
+			},
+			"size": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"filesystem": &graphql.Field{
+				Type: graphql.String,
+			},
+			"server_uuid": &graphql.Field{
+				Type: graphql.String,
+			},
+			"use_type": &graphql.Field{
+				Type: graphql.String,
+			},
+			"user_uuid": &graphql.Field{
+				Type: graphql.String,
+			},
+			"created_at": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+>>>>>>> 6d18aed (update architecture):action/graphql/volumeType.go
